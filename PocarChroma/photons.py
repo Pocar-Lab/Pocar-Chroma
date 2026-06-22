7d7cfca010d12628a8ab20e7a0aaf4dbf28e7703
samalbury <salbury@umass.edu>
2026-06-22 11:34:10 -0400

    Created a function, tprint, which will print only if test_mode = True This is for testing, and test_mode defaults to False
    Started to move/make docstrings
    added two tprint statements to tallies_write for debug purposes

--- PocarChroma/photons.py
+++ PocarChroma/photons.py
@@ -16,6 +16,11 @@ from .analysis_manager import analysis_manager
 '''
 ================ START AUXILLARY FUNCTIONS ===============
 '''
+# Set this parameter to true to enable more information printed out
+test_mode = False
+
+# tprint will print only if test mode is set to True
+tprint = print if test_mode else lambda *a, **k: None
 
 '''
 ================= START PROPAGATION CODE =================
@@ -122,30 +127,33 @@ def propagate(
 ================= START FILE SAVING CODE =================
 '''
 
-'''
-Makes an HDF5 file with the desired header information
 
-You can pass anything under 64kb into attributes and it will be saved along with the file. 
-This includes things such as a notes section
-To read attributes, use
-
-f.attrs.get[<attribute_name>].
-
-It also creates two datasets, one called tallies and one called tracks
-'''
 def make_HDF5_file(
     file_path, 
     attributes:dict,
 
-    # Note, specifying dataset shape will ensure that file creation goes smoothly.
-    # The tracks dataset has the shape (<number of steps> + 1, <number of tracks to be saved>, 3)
+
     tracks_shape,    # The shape of the tracks dataset, in standard numpy notation.
 
-    # You should also specify the total number of rows (photons) in the tallies arrays
     tallies_rows:int,
     # and the tallies columns
     tallies_columns
     ):
+    '''
+    Makes an HDF5 file with the desired header information
+
+    You can pass anything under 64kb into attributes and it will be saved along with the file. 
+    This includes things such as a notes section
+    To read attributes, use
+
+    f.attrs.get[<attribute_name>].
+
+    It also creates two datasets, one called tallies and one called tracks
+
+    Note, specifying dataset shape will ensure that file creation goes smoothly.
+    The tracks dataset has the shape (<number of steps> + 1, <number of tracks to be saved>, 3)
+    You should also specify the total number of rows (photons) in the tallies arrays
+    '''
     # I could in theory make it so the HDF5 file is configured to be dynamic, but that would take extra work that doesn't seem worth it right now
 
     save_dir = file_path.rsplit('/', 1)[0]
@@ -195,9 +203,20 @@ def tallies_write(
     file_path:str,
     tallies_dict:dict,
 ):
+    '''
+    :param file_path: The path to the previously created hdf5 file
+    :type file_path: str
+    :param tallies_dict: A dictionary of numpy arrays
+    :type tallies_dict: dict
+    '''
+    
+    tprint('The tallies dictionary is:')
+    tprint(tallies_dict)
+
     with h5py.File(file_path, 'r+') as f:
         ds = f['tallies']
         next_row = ds.attrs['next_writable']
+
 
         # Get the end row by getting the length of the first array in the dict
         end_row = next_row + len(next(iter(tallies_dict.values())))
